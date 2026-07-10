530a57bd148cb328d5506f1ba0beef0997988cd7
priyanshu solanki <131973299+Priyanshu91930@users.noreply.github.com>
2026-07-10 11:56:18 +0530

    Update STRING_SESSION in config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 API_ID = int(environ.get("API_ID", "27686895"))
 API_HASH = environ.get("API_HASH", "0e996bd3891969ec5dfebf8bb3e39e94")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8583744005:AAHxiBsiOts0XT-yORJYuQptiYfOc2J5aAk")
-STRING_SESSION = environ.get("STRING_SESSION", "BQGmd-8Ajv1Wbi98dq1zvhV3MPFbWNgHZafM2wFTGfU4EAEP0osrDBViCLoMGi0Cq0Q9yLG8dhXhM3v-HzoWxol7qBmrt7KRNt5Dbvs-mUhYBXxZpau6kSzip4CCpxmqLP6ONpd1_AChdONmQ0cdyICh1WIoz6TwWA15tsp0Q-9aWMaQ213wJ5Jf7oBQXTzHnkqi-8b_a0n6yCfuPXb3vJi23zoxB8d5psjFMwlnVBi4w_zd3ivk41NLzJT8i6gaEdnp2BNa_H3RXPsD808zwZgaoXPlGS2BPWxDznS-6XRSUaLVrSFDr7xGsYA-3gIvsMzDEuOgtbsQVa58HSRS4GrIpXIPHwAAAABKU4XBAA") # User session for bot interaction
+BOT_TOKEN = environ.get("BOT_TOKEN", "8811056986:AAEhjHZjujjwhZNII97Y5-5yrvFS19pz6KQ")
+STRING_SESSION = environ.get("STRING_SESSION", "BQGmd-8ASsYhTQ9_9JwftQk0jn_xb63j-_uoY97MFXuQwTUD-KjR9LYtek3Jp4m7CFnvEF2TqEPVxqv1K78OVFa2wCuG4uiPDwMSmyjRxm38dpTg9M7rSAtTzueWwg1uiQIboFwknGrH2-5rO6au85JYHJVZNKSwDeOCKx2lDRsMQptXqJucEjs6-MkX_DuYP9FsVvCJcQe1t5aoJXHFPK2qwXHITH5k5ePejysKTvL21NXc1S6hyVVJzMuTluehUmXct09yF1_1eYO0gfnhYOxkQwxQYTaF6gQpqjSEEe0XUiasIk5cdFOjUrHmtKaEMxksRNSTexbbXhVVF4uzJVohGcTLegAAAABKU4XBAA") # User session for bot interaction
 ADMINS = [1246987713]
 DB_URI = environ.get("DB_URI", "mongodb+srv://anihubyt:Zxcvbnmm9193@cluster0.qv5tu12.mongodb.net/?appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "banana_bot")
